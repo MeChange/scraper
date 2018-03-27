70641e877820b11746928102889f55de61b96dcb
shinzenkoru <empongpat@gmail.com>
2018-03-27 09:31:29 +0700

    Refactor methods according to Effective Go

--- common/currency.go
+++ common/currency.go
@@ -21,14 +21,14 @@ import (
 
 // A Currency carries a quote, buy rate, and sell rate
 type Currency struct {
-	Quote string
+	Quote             string
 	BuyRate, SellRate float64
 }
 
-// GetBuyRateString get buy rate as formatted string
-func (curr *Currency) GetBuyRateString() (string, error) {
+// BuyRateString get buy rate as formatted string
+func (curr *Currency) BuyRateString() (string, error) {
 	if curr.BuyRate <= 0 {
-		return "", errors.New("Buy rate is less than or equal to zero") 
+		return "", errors.New("Buy rate is less than or equal to zero")
 	}
 	// For currency that rate is >= 1.0 has 2 decimal points e.g. USD
 	if curr.BuyRate >= 1.0 {
@@ -38,8 +38,8 @@ func (curr *Currency) GetBuyRateString() (string, error) {
 	return strconv.FormatFloat(curr.BuyRate, 'f', 4, 64), nil
 }
 
-// GetSellRateString get sell rate as formatted string
-func (curr *Currency) GetSellRateString() (string, error) {
+// SellRateString get sell rate as formatted string
+func (curr *Currency) SellRateString() (string, error) {
 	if curr.SellRate <= 0 {
 		return "", errors.New("Sell rate is less than or equal to zero")
 	}
